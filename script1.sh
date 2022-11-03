0c9288c6dc4207fcf4b729f26c2db5c0f3c04f04
jwagnervaz <67094529+jwagnervaz@users.noreply.github.com>
2022-11-03 17:49:32 -0300

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ -f YVdq ];then
-	lz4 -B6 --content-size -f YVdq recovery2.img
+if [ -f recovery.lz4.img ];then
+	lz4 -B6 --content-size -f recovery.lz4.img recovery.img
 fi
 
 off=$(grep -ab -o SEANDROIDENFORCE recovery.img |tail -n 1 |cut -d : -f 1)
-dd if=recovery2.img of=recovery.img bs=4k count=$off iflag=count_bytes
+dd if=recovery.img of=recovery.img bs=4k count=$off iflag=count_bytes
 
 if [ ! -f phh.pem ];then
     openssl genrsa -f4 -out phh.pem 4096
